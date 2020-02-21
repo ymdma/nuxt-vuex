8c1c9669974ebd747222940e18f65b9776faafc1
ymdma <m.yamada1985@gmail.com>
2020-02-21 17:39:36 +0900

    store_js

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
+
   <div class="container">
     <div>
       <p>{{ $store.state.message }}</p>
     </div>
   </div>
+
 </template>
 
 <script>
+
 export default {
 }
+
 </script>
 
 <style>
+
 </style>
