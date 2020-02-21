9a28fbe8b84dcf45a368e0c9fd9d02e10723fe50
ymdma <m.yamada1985@gmail.com>
2020-02-21 17:38:53 +0900

    f-c

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div>
+      <p>{{ $store.state.message }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ store/index.js
@@ -0,0 +1,19 @@
+import Vuex from 'vuex' // vuexをインポートして使えるようにする
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: function() { // state => ストアで管理する値,stateの値は常にfunctionにしなくてはならない ,コンポーネントのdataオプションみたいなもの
+      return{ //オブジェクトを返す
+        message: 'Hello Vuex!'
+      }
+    }
+  })
+}
+// stateに値を保管する処理
+// message => 画面に表示するテキストを格納するための値 messageでなくてもよい
+
+export default createStore
+// createStoreをexport defaultでエクスポートして使えるようにする。
+// （createStore関数を定義して、プログラムから使えるようにする）
+
+// ===> pages/index.vue
